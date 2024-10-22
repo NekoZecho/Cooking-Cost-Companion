a71e62ed232f5cb9b1f3d410836b33fae58a2951
NekoZecho <zacharymunn07@gmail.com>
2024-10-22 09:29:04 -0700

    UI Test

---
+++ Assets/Scripts/ThemeSwap.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ThemeSwap : MonoBehaviour
+{
+    [SerializeField]
+    Image BackGround;
+    [SerializeField]
+    Sprite BG_R;
+    [SerializeField]
+    Sprite BG_G;
+    [SerializeField]
+    Sprite BG_B;
+    float swap;
+    void Start()
+    {
+        BackGround.sprite = BG_R;
+        swap = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (swap == 0)
+            {
+                BackGround.sprite = BG_G;
+                swap = 1;
+            }
+            else if (swap == 1)
+            {
+                BackGround.sprite = BG_B;
+                swap = 2;
+            }
+            else
+            {
+                BackGround.sprite = BG_R;
+                swap = 0;
+            }
+        }
+    }
+}
